181c6f2b12af7b838ca567c8f389263b02c04634
Dongho Sim <dhsim86@gmail.com>
2024-11-09 19:08:22 +0900

    token: rename

--- token/embedding_text.py
+++ token/embedding_text.py
@@ -13,18 +13,18 @@ print("str2idx: ", str2idx)
 print("idx2str: ", idx2str)
 
 # 토큰을 토큰 아이디로 변환
-input_ids = [str2idx[word] for word in input_text_list]
-print("input_ids: ", input_ids)
+token_ids = [str2idx[word] for word in input_text_list]
+print("token_ids: ", token_ids)
 
 embedding_dim = 16
 embed_layer = nn.Embedding(len(str2idx), embedding_dim) # 사전 크기가 5이고, 16차원의 임베딩을 생성하는 임베딩 레이어 생성
 
-token_embeddings = embed_layer(torch.tensor(input_ids))
+token_embeddings = embed_layer(torch.tensor(token_ids))
 token_embeddings = token_embeddings.unsqueeze(0)
 
 max_positions = 12 # 최대 토큰 수
 position_embed_layer = nn.Embedding(max_positions, embedding_dim) # 위치 임베딩 레이어 생성
-position_ids = torch.arange(len(input_ids), dtype=torch.long).unsqueeze(0)
+position_ids = torch.arange(len(token_ids), dtype=torch.long).unsqueeze(0)
 
 position_encodings = position_embed_layer(position_ids)
 
