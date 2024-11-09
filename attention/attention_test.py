7a5a7c34d96f88827356841621a7c7be2101bff1
Dongho Sim <dhsim86@gmail.com>
2024-11-09 23:22:27 +0900

    token: simulates attention.

---
+++ attention/attention_test.py
@@ -0,0 +1,50 @@
+from tokens import embedding_text
+
+from math import sqrt
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# 쿼리, 키, 값 벡터 생성
+def get_qkv(input_embedding: str):
+    embedding_dim = 16
+    head_dim = 16
+
+    # 쿼리, 키, 값을 계산하기 위한 변환
+    weight_q = nn.Linear(embedding_dim, head_dim)
+    weight_k = nn.Linear(embedding_dim, head_dim)
+    weight_v = nn.Linear(embedding_dim, head_dim)
+
+    # 변환수행
+    queries = weight_q(input_embedding)
+    keys = weight_k(input_embedding)
+    values = weight_v(input_embedding)
+
+    print(f"queries: {queries}")
+    print(f"keys: {keys}")
+    print(f"values: {values}")
+
+    return queries, keys, values
+
+
+def compute_attention(quries, keys, values, is_causal=False):
+    print("---------------compute_attention-----------------")
+    dim_k = quries.size(-1)
+
+    # 쿼리와 키를 곱하고, 분산이 커지는 것을 방지하기 위해 임베딩 차원 수의 제곱근으로 나눈다.
+    scores = queris @ keys.transpose(-2, -1) / sqrt(dim_k)
+    print(f"scores: {scores}")
+
+    # 쿼리와 키를 곱해 계산한 score를 합이 1이 되도록 소프트맥스를 취해 가중치로 바꾼다.
+    weight = F.softmax(scores, dim=-1)
+    print(f"weight: {scores}")
+
+    # 가중치와 값을 곱해 입력과 동일한 형태의 출력
+    return weight @ values
+
+
+if __name__ == '__main__':
+    queris, keys, values = get_qkv(embedding_text.get_input_embeddings())
+    result = compute_attention(queris, keys, values)
+
+    print(f"result: {result}")
---
+++ tokens/embedding_text.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+def get_input_embeddings() -> str:
+    # 띄어쓰기 단위로 분리
+    input_text = "나는 최근 파리 여행을 다녀왔다"
+    input_text_list = input_text.split()
+    print("input_text_list: ", input_text_list)
+
+    # 토큰 -> 아이디 딕셔너리와 아이디 -> 토큰 딕셔너리 만들기
+    str2idx = {word: idx for idx, word in enumerate(input_text_list)}
+    idx2str = {idx: word for idx, word in enumerate(input_text_list)}
+    print("str2idx: ", str2idx)
+    print("idx2str: ", idx2str)
+
+    # 토큰을 토큰 아이디로 변환
+    token_ids = [str2idx[word] for word in input_text_list]
+    print("token_ids: ", token_ids)
+
+    embedding_dim = 16
+    embed_layer = nn.Embedding(len(str2idx), embedding_dim)  # 사전 크기가 5이고, 16차원의 임베딩을 생성하는 임베딩 레이어 생성
+
+    token_embeddings = embed_layer(torch.tensor(token_ids))
+    token_embeddings = token_embeddings.unsqueeze(0)
+
+    max_positions = 12  # 최대 토큰 수
+    position_embed_layer = nn.Embedding(max_positions, embedding_dim)  # 위치 임베딩 레이어 생성
+    position_ids = torch.arange(len(token_ids), dtype=torch.long).unsqueeze(0)
+
+    position_encodings = position_embed_layer(position_ids)
+
+    input_embeddings = token_embeddings + position_encodings
+
+    print(f"token_embeddings: {token_embeddings}")
+    print(f"position_encodings: {position_encodings}")
+    print(f"input_embeddings: {input_embeddings}")
+
+    return input_embeddings
+
+
+if __name__ == '__main__':
+    get_input_embeddings()
