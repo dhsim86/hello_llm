7a648239fcfe426c77a861ac939c359cb3716795
Dongho Sim <dhsim86@gmail.com>
2025-01-20 10:53:05 +0900

    주석 update

--- huggingface/load_tokenizers/usage.py
+++ huggingface/load_tokenizers/usage.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     tokenized = tokenizer("토크나이저는 텍스트를 토큰 단위로 나눈다.")
-    print(tokenized)
+    print(f"tokenized: {tokenized}")
 
     # 토큰 ID to 토큰
-    print(tokenizer.convert_ids_to_tokens(tokenized['input_ids']))
+    print(f"토큰 ID to 토큰: {tokenizer.convert_ids_to_tokens(tokenized['input_ids'])}")
 
     # 토큰 ID들을 다시 텍스트로 디코드
-    print(tokenizer.decode(tokenized['input_ids']))
+    print(f"토큰 ID를 다시 텍스트로 디코드: {tokenizer.decode(tokenized['input_ids'])}")
 
     # 디코드할 때 특수 토큰 제외
-    print(tokenizer.decode(tokenized['input_ids'], skip_special_tokens=True))
+    print(f"토큰 ID를 다시 텍스트로 디코드(특수 토큰 제외): {tokenizer.decode(tokenized['input_ids'], skip_special_tokens=True)}")
