83e2e65eb10a43fe0c487ca8f3e08aa6fa3c9d6c
Dongho Sim <dhsim86@gmail.com>
2025-02-22 20:34:50 +0900

    update

--- memory/experiment.py
+++ memory/experiment.py
@@ -55,6 +55,7 @@ if __name__ == '__main__':
     print_gpu_utilization()
 
     for batch_size in [4, 8, 16]:
-        gpu_memory_experiment(batch_size=batch_size, gradient_accumulation_steps=4)
+        # batch_size * gradient_accumulation_steps 크기 만큼의 배치 크기 효과를 얻을 수 있다.
+        gpu_memory_experiment(batch_size=batch_size, gradient_accumulation_steps=1, gradient_checkpointing=True)
 
         torch.cuda.empty_cache()
