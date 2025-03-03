2fb23980ffc1d15bf7e75481345befe71db7a149
Dongho Sim <dhsim86@gmail.com>
2025-03-03 17:40:37 +0900

    refactor

---
+++ text2sql/openai_evaluation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# OpenAI Encodings: https://cookbook.openai.com/examples/how_to_count_tokens_with_tiktoken
+# OpenAI RateLimits: https://platform.openai.com/docs/guides/rate-limits?tier=free#free-tier-rate-limits
+
+export OPENAI_API_KEY=sk-1234567890abcdef123456789
+
+# gpt-4o-mini ratelimit
+# - RPM: 3
+# - TPM: 40,000
+python api_request_parallel_processor.py \
+  --requests_filepath {요청 파일 경로} \
+  --save_filepath {생성할 결과 파일 경로} \
+  --request_url https://api.openai.com/v1/chat/completions \
+  --max_requests_per_minute 2 \
+  --max_tokens_per_minute 30000 \
+  --token_encoding_name o200k_base \
+  --max_attempts 5 \
+  --logging_level 20python api_re
--- text2sql/utils.py
+++ text2sql/utils.py
@@ -1,17 +1,35 @@
-from datasets import load_dataset
+# DDL 및 사용자 요청사항(question), 정답 SQL(query)을 통해 프롬프트 생성
+# 학습할 때는 정답 SQL을 넣고 학습
+# 추론할 때는 SQL을 넣지 않는다.
+def make_prompt(ddl, question, query=''):
+    prompt = f"""당신은 SQL을 생성하는 SQL 봇입니다. DDL의 테이블을 활용한 Question을 해결할 수 있는 SQL 쿼리를 생성하세요.
 
-from prompt import make_prompt
-from hf_pipe import hf_pipe
+### DDL:
+{ddl}
 
-import json
-from pathlib import Path
+### Question:
+{question}
+
+### SQL:
+{query}"""
+    return prompt
 
 
+# 입력한 데이터프레임을 순회하면서 평가를 위해 사용할 프롬프트 생성
+# 프롬프트를 jsonl 파일에 기록
+# 프롬프트의 내용
+#   - DDL과 Question을 바탕으로 LLM이 생성한 SQL과 정답 SQL이 동일한 기능을 하는지 평가
+#   - 판단 결과는 JSON 형식으로 resolve_yn 이라는 프로퍼티에 yes / no 값으로 받는다.
 def make_requests_for_gpt_evaluation(df, filename, dir='requests'):
+    from pathlib import Path
+    import json
+
     if not Path(dir).exists():
         Path(dir).mkdir(parents=True)
+
     prompts = []
     for idx, row in df.iterrows():
+        # 프롬프트 생성
         prompts.append(
             """Based on below DDL and Question, evaluate gen_sql can resolve Question. If gen_sql and gt_sql do equal job, return "yes" else return "no". Output JSON Format: {"resolve_yn": ""}""" + f"""
 
@@ -21,8 +39,11 @@ gt_sql: {row['answer']}
 gen_sql: {row['gen_sql']}"""
         )
 
-    jobs = [{"model": "gpt-4-turbo-preview", "response_format": {"type": "json_object"},
+    # 모델 이름 및 출력 타입을 지정하고, 프롬프트를 system role로 지정
+    jobs = [{"model": "gpt-4o", "response_format": {"type": "json_object"},
              "messages": [{"role": "system", "content": prompt}]} for prompt in prompts]
+
+    # jsonl 형식으로 요청 정보를 저장
     with open(Path(dir, filename), "w") as f:
         for job in jobs:
             json_string = json.dumps(job)
@@ -30,6 +51,9 @@ gen_sql: {row['gen_sql']}"""
 
 
 if __name__ == '__main__':
+    from datasets import load_dataset
+    from hf_pipe import hf_pipe
+
     # 데이터셋 불러오기
     df = load_dataset("shangrilar/ko_text2sql", "origin")['test']
     df = df.to_pandas()
