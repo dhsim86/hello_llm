b8921df9fac0b7d0d371ea9659a41f6026ca95c7
Dongho Sim <dhsim86@gmail.com>
2025-03-03 18:00:07 +0900

    refactor

--- text2sql/utils/model.py
+++ text2sql/utils/model.py
@@ -7,12 +7,16 @@ from peft import LoraConfig, get_peft_model
 def make_inference_pipeline(model_id):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
+    ###################################################################
+    # NVIDIA GPU
     # the bitsandbytes library only works on CUDA GPU.
     # https://huggingface.co/docs/bitsandbytes/v0.42.0/installation#installation
     # https://github.com/huggingface/transformers/issues/23970#issuecomment-1598861262
     if torch.cuda.is_available():
         model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", load_in_4bit=True,
                                                      bnb_4bit_compute_dtype=torch.float16)
+    ###################################################################
+    # macOS with MPS
     elif torch.backends.mps.is_available():
         model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map="mps")
 
