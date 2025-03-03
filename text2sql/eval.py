38ae300c0add13051ca1b5e109ad07d8a32cc27a
Dongho Sim <dhsim86@gmail.com>
2025-03-03 18:10:40 +0900

    refactor

--- text2sql/eval.py
+++ text2sql/eval.py
@@ -7,13 +7,15 @@ if __name__ == '__main__':
     model_id = 'beomi/Yi-Ko-6B'
     model_pipe = make_inference_pipeline(model_id)
 
-    # 데이터셋 불러오기
+    # 평가 데이터셋 불러오기
     df = load_dataset("shangrilar/ko_text2sql", "origin")['test']
+
     df = df.to_pandas()
     for idx, row in df.iterrows():
         # make_prompt를 통해 LLM 추론에 사용할 프롬프트 생성
         prompt = make_prompt(row['context'], row['question'])
         df.loc[idx, 'prompt'] = prompt
+
     # 모델로 추론하여 SQL을 생성 후 저장
     gen_sqls = model_pipe(df['prompt'].tolist(), do_sample=False,
                           return_full_text=False, max_length=512, truncation=True)
