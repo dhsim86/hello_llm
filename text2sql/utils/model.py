fc90a69195a48ac383ebbd8af719a9b637865347
Dongho Sim <dhsim86@gmail.com>
2025-03-09 18:09:03 +0900

    model load 분리

--- text2sql/utils/model.py
+++ text2sql/utils/model.py
@@ -5,6 +5,13 @@ from peft import LoraConfig, get_peft_model
 
 # 모델 ID를 받아 토크나이저 및 모델을 로드 후 파이프라인으로 리턴
 def make_inference_pipeline(model_id):
+    model, tokenizer = load_model_and_tokenizer(model_id)
+
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    return pipe
+
+
+def load_model_and_tokenizer(model_id):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     ###################################################################
@@ -18,7 +25,7 @@ def make_inference_pipeline(model_id):
     ###################################################################
     # macOS with MPS
     elif torch.backends.mps.is_available():
-        model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map="mps")
+        model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map="mps")
 
         ############################
         # LoRA 적용
@@ -47,5 +54,4 @@ def make_inference_pipeline(model_id):
     else:
         model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
 
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    return pipe
+    return model, tokenizer
